9cb165248c0794b4beb63b5d33fe3dd6aeebeede
UssCringe <david.petur@gmail.com>
2023-01-22 05:24:40 +0100

    Add project files.

---
+++ WavingManV2/RightHandedMan.cs
@@ -0,0 +1,21 @@
+﻿namespace WavingMan;
+
+internal class RightHandedMan : Man
+{
+    public RightHandedMan(int x, int y, int dx, int dy) : base(x, y, dx, dy)
+    {
+            
+    }
+
+    public override void Show()
+    {
+        SetCursorTop();
+        SetCursorLeft();
+        Console.WriteLine(_shouldWaveNextTime ? " o/" : " o");
+        SetCursorLeft();
+        Console.WriteLine(_shouldWaveNextTime ? "/|" : " |");
+        SetCursorLeft();
+        Console.Write("/ \\");
+        _shouldWaveNextTime = !_shouldWaveNextTime;
+    }
+}
